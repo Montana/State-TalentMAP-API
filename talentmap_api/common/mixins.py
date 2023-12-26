6bcc98158448b59162d96a97047aedacd13822d7
Michael Mendy <montana@linux.com>
2023-12-26 10:06:04 -0800

    Upgrade to `PEP8` standards.

--- talentmap_api/common/mixins.py
+++ talentmap_api/common/mixins.py
@@ -1,18 +1,18 @@
 class ActionDependentSerializerMixin(object):
-    '''
+    """
     Supports differentiating serializers across actions
-    '''
+    """
 
     def get_serializer_class(self):
-        return self.serializers.get(self.action, self.serializers['default'])
+        return self.serializers.get(self.action, self.serializers["default"])
 
 
 class FieldLimitableSerializerMixin(object):
-    '''
+    """
     Supports limiting the return fields via the specified include and exclude
     query parameters from a request by passing the data into the serializer via
     the serializer context. Only works with PrefetchedSerializer descendants
-    '''
+    """
 
     def get_serializer_context(self):
         context = super(FieldLimitableSerializerMixin, self).get_serializer_context()
@@ -22,9 +22,19 @@ class FieldLimitableSerializerMixin(object):
 
         # Check query params for "include", which are included fields
         if include_param_name in self.request.query_params:
-            context = {**context, "override_fields": self.request.query_params.get(include_param_name).split(',')}
+            context = {
+                **context,
+                "override_fields": self.request.query_params.get(
+                    include_param_name
+                ).split(","),
+            }
         # Check query params for "exclude", which are excluded fields
         if exclude_param_name in self.request.query_params:
-            context = {**context, "override_exclude": self.request.query_params.get(exclude_param_name).split(',')}
+            context = {
+                **context,
+                "override_exclude": self.request.query_params.get(
+                    exclude_param_name
+                ).split(","),
+            }
 
         return context
